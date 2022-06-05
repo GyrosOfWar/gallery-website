f40b9873c3acc9ec7c0376aaa5390594cd04ab9c
Martin Tomasi <martin.tomasi@gmail.com>
2022-06-05 10:25:38 +0200

    add tailwind

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use axum::{
-    body,
     http::StatusCode,
-    response::{Html, IntoResponse, Response},
-    routing::get,
+    response::{Html, IntoResponse},
+    routing::{get, get_service},
     Extension, Router,
 };
-use color_eyre::Report;
-use std::{fmt, net::SocketAddr, sync::Arc};
+use sqlx::SqlitePool;
+use std::{net::SocketAddr, sync::Arc};
 use tera::Tera;
-use tracing::warn;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing_subscriber::EnvFilter;
 
 pub struct Context {
     pub tera: Tera,
+    pub sql: SqlitePool,
 }
 
 impl Context {
@@ -24,26 +24,33 @@ impl Context {
     }
 }
 
+async fn handle_error(_err: std::io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
+}
+
 async fn index(Extension(context): Extension<Arc<Context>>) -> impl IntoResponse {
     context.render_template("index.html")
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::new(
             std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
         ))
-        .with(tracing_subscriber::fmt::layer())
         .init();
+
     let tera = Tera::new("templates/**/*")?;
-    let context = Arc::new(Context { tera });
+    let sql = SqlitePool::connect("sqlite::memory").await?;
+    let context = Arc::new(Context { tera, sql });
     let app = Router::new()
         .route("/", get(index))
-        .layer(Extension(context));
+        .fallback(get_service(ServeDir::new("public")).handle_error(handle_error))
+        .layer(Extension(context))
+        .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
+    tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
